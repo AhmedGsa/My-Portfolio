e284a7dffa47a51419748b3064823b1cfc0afe18
AhmedGsa <a_gouasmia@estin.dz>
2024-08-07 23:37:05 +0100

    added company images in experience section

--- src/components/experience/Experience.tsx
+++ src/components/experience/Experience.tsx
@@ -7,11 +7,11 @@ export default function Experience() {
         <p>I have a range of experience in the tech industry.</p>
         
         <div className={classes.jobs}>
-            <Job title="Web Developer" company="Fiverr" date="Juin 2022 - Present" />
+            <Job title="Web Developer" company="Fiverr" date="Juin 2022 - Present" image='/images/fiverr.png' />
             <hr />
-            <Job title="Backend Developer" company="Createch" date="July 2023 - April 2024" />
+            <Job title="Backend Developer" company="Createch" date="July 2023 - April 2024" image='/images/createch.jpeg' />
             <hr />
-            <Job title="Backend Developer" company="Megasoft Office" date="May 2024 - Present" />
+            <Job title="Backend Developer" company="Megasoft Office" date="May 2024 - Present" image='/images/megasoft.jpeg' />
         </div>
         
     </section>
--- src/components/experience/Job.tsx
+++ src/components/experience/Job.tsx
@@ -1,11 +1,15 @@
+import Image from 'next/image';
 import classes from './Experience.module.css';
 
-export default function Job({ title, company, date } : { title: string, company: string, date: string }) {
+export default function Job({ title, company, date, image } : { title: string, company: string, date: string, image: string }) {
     return <div className={classes.job}>
         <h2>{date}</h2>
         <div className={classes.right}>
-            <h1>{title}</h1>
-            <h2>{company}</h2>
+            <Image src={image} width={100} height={100} alt="Experience" />
+            <div className={classes.info}>
+                <h1>{title}</h1>
+                <h2>{company}</h2>
+            </div>
         </div>
     </div>
 }
