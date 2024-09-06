c646aedf388176dfc9b888e09f7c952efcece9bf
AhmedGsa <a_gouasmia@estin.dz>
2024-09-06 17:14:37 +0100

    fixed slider animation

--- src/components/hero/HeroHeader.tsx
+++ src/components/hero/HeroHeader.tsx
@@ -3,22 +3,13 @@ import classes from "./HeroHeader.module.css";
 
 export default function HeroHeader() {
     return <header className={classes['hero-header']}>
-        <h1>Software Development By Ahmed Gouasmia
-            <div className={classes['service-slider']}>
-                <div className={classes['service-text-content']}>
-                    <div className={classes['service-text-slide']}>
-                        <h2>Fullstack Developer</h2>
-                        <Image src="/images/north-star.png" alt="North Star Image" width='50' height='50' />
-                        <h2>Artificial Intelligence and Data Science Student</h2>
-                    </div>
-                    <div className={classes['service-text-slide']}>
-                        <h2>Fullstack Developer</h2>
-                        <Image src="/images/north-star.png" alt="North Star Image" width='50' height='50' />
-                        <h2>Artificial Intelligence and Data Science Student</h2>
-                    </div>
-                </div>
+        <h1>Software Development By Ahmed Gouasmia</h1>
+        <div className={classes['service-slider']}>
+            <div className={classes['service-text-slide']}>
+                <h2>Fullstack Developer</h2>
+                <Image src="/images/north-star.png" alt="North Star Image" width='50' height='50' />
+                <h2>Artificial Intelligence and Data Science Student</h2>
             </div>
-        </h1>
-        
+        </div>
     </header>
 }
